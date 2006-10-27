14443e0e4e97783bdde64e59d28722c83c128501
John Wilger <johnwilger@gmail.com>
2006-10-27 05:10:57 +0000

    r40@Woden (orig r36):  jwilger | 2005-10-11 06:28:57 -0700  Fixed sorting of '@my_projects' for MainController#dashboard
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@38 1861e259-8220-0410-84d7-1f84cab42028

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,6 @@ class MainController < ApplicationController
   # The "main" view of the application for each user. Shows summary of projects
   # and recent activity on those projects that the user is associated with.
   def dashboard
-    @my_projects = self.current_user.projects
+    @my_projects = self.current_user.projects.sort { |a, b| a.name <=> b.name }
   end
 end
