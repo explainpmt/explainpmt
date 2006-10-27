099320b54bdd937c3ed9c962c7e0edfb41a74eca
John Wilger <johnwilger@gmail.com>
2006-10-27 05:46:53 +0000

    r159@Woden (orig r155):  jwilger | 2006-02-06 10:50:12 -0800  refactored test fixtures for SessionControllerTest
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@157 1861e259-8220-0410-84d7-1f84cab42028

--- test/functional/session_controller_test.rb
+++ test/functional/session_controller_test.rb
@@ -5,9 +5,10 @@ require 'session_controller'
 class SessionController; def rescue_action(e) raise e end; end
 
 class SessionControllerTest < Test::Unit::TestCase
+  fixtures ALL_FIXTURES
+  
   def setup
-    User.destroy_all
-    create_common_fixtures :user_one
+    @user_one = User.find 2
     @controller = SessionController.new
     @request = ActionController::TestRequest.new
     @response = ActionController::TestResponse.new
@@ -20,31 +21,31 @@ class SessionControllerTest < Test::Unit::TestCase
 
   def test_authenticate
     post :authenticate, 'username' => @user_one.username,
-         'password' => @user_one.password
-    assert_equal @user_one, session[:current_user]
+      'password' => @user_one.password
+    assert_equal @user_one, session[ :current_user ]
     assert_redirected_to :controller => 'dashboard', :action => 'index'
   end
 
   def test_authenticate_with_return_to
-    @request.session[:return_to] = '/foo/bar'
+    @request.session[ :return_to ] = '/foo/bar'
     post :authenticate, 'username' => @user_one.username,
-         'password' => @user_one.password
+      'password' => @user_one.password
     assert_redirect_url 'http://test.host/foo/bar'
   end
 
   def test_authenticate_failed
     post :authenticate
     assert_redirected_to :controller => 'session', :action => 'login'
-    assert_nil session[:current_user]
+    assert_nil session[ :current_user ]
     assert_equal "You entered an invalid username and/or password.",
-                 flash[:error]
+      flash[ :error ]
   end
 
   def test_logout
-    @request.session[:current_user] = @user_one
+    @request.session[ :current_user ] = @user_one
     get :logout
-    assert_nil session[:current_user]
+    assert_nil session[ :current_user ]
     assert_redirected_to :controller => 'session', :action => 'login'
-    assert_equal "You have been logged out.", flash[:status]
+    assert_equal "You have been logged out.", flash[ :status ]
   end
 end
