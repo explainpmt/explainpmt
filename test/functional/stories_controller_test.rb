e1191871c919df762b5449661333bc3f8fab5d8f
John Wilger <johnwilger@gmail.com>
2006-10-27 05:44:19 +0000

    r150@Woden (orig r146):  rmannl | 2006-01-11 22:24:27 -0800  Added test coverage for the changes in MainMenu I made (r138-r140)
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@148 1861e259-8220-0410-84d7-1f84cab42028

--- test/functional/stories_controller_test.rb
+++ test/functional/stories_controller_test.rb
@@ -156,4 +156,12 @@ class StoriesControllerTest < Test::Unit::TestCase
     assert flash[:status]
     assert_nil Story.find(@story_one.id).owner
   end
+  
+  def test_main_menu_has_correct_current_link
+    # new/edit/view story should have no selected item (class="current") in the MainMenu
+    for action in [:new, :show, :edit]
+      get action, 'id' => @story_one.id, 'project_id' => @project_one.id
+      assert_no_tag :tag => 'a', :attributes => { :class => 'current' }, :ancestor => { :tag => 'ul', :attributes => { :id => 'MainMenu' } }
+    end
+  end
 end
