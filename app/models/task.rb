6e44dd3981f46ef62e24efaf416392cbfc27e326
Jake Dempsey <angelo0000@gmail.com>
2006-10-29 08:49:13 +0000

    added foreign key to remove dep warning
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@260 1861e259-8220-0410-84d7-1f84cab42028

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :story
-  belongs_to :owner, :class_name => 'User'
+  belongs_to :owner, :class_name => 'User', :foreign_key => 'user_id'
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => "story_id"
 end
