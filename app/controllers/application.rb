5c1d404ba3e46ebd3ee5329ac64a55f93a3a6924
Jake Dempsey <angelo0000@gmail.com>
2006-10-29 08:51:03 +0000

    updated wizard controller
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@262 1861e259-8220-0410-84d7-1f84cab42028

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -169,28 +169,27 @@ class WizardController < ApplicationController
     @page_title = "New #{mymodel}"
   end
 
-  #Not used yet.  
-#  def create
-#    object_to_create = mymodel.new(params[:object])
-#    object_to_create.project = @project if object_to_create.has_attribute?(:project_id)
-#    if object_to_create.valid?
-#      object_to_create.save
-#      if mymodel.name == "Project" && params[:add_me] == '1'
-#        session[:current_user].projects << object_to_create
-#      end
-#      flash[:status] = "#{mymodel} \"#{object_to_create.name}\" has been saved."
-#      render 'layouts/refresh_parent_close_popup'
-#    else
-#      session[:new_object] = object_to_create
-#      redirect_to :action => 'new',:project_id => @project.id
-#    end
-#  end
+  def create
+    object_to_create = mymodel.new(params[:object])
+    object_to_create.project = @project if object_to_create.has_attribute?(:project_id)
+    if object_to_create.valid?
+      object_to_create.save
+      if mymodel.name == "Project" && params[:add_me] == '1'
+        session[:current_user].projects << object_to_create
+      end
+      flash[:status] = "#{mymodel} \"#{object_to_create.name}\" has been saved."
+      render :template => 'layouts/refresh_parent_close_popup'
+    else
+      session[:new_object] = object_to_create
+      redirect_to :action => 'new',:project_id => @project.id
+    end
+  end
   
   def update
     ojbect_to_edit = mymodel.find(params[:id])
     if ojbect_to_edit.update_attributes(params[:object])
       flash[:status] = "Changes to \"#{ojbect_to_edit.name}\" have been been saved."
-      render 'layouts/refresh_parent_close_popup'
+      render :template => 'layouts/refresh_parent_close_popup'
     else
       session[:edit_object] = ojbect_to_edit
       redirect_to :action => 'edit', :id => ojbect_to_edit.id,
@@ -201,13 +200,12 @@ class WizardController < ApplicationController
   def show
     @object = mymodel.find(params[:id])
   end
-  
-  #Not used yet. 
-#  def delete
-#    object_selected = mymodel.find(params[:id])
-#    object_selected.destroy
-#    flash[:status] = "#{mymodel.name} \"#{object_selected.name}\" has been deleted."
-#    redirect_to :action => 'index', :project_id => @project.id
-#  end
+   
+  def delete
+    object_selected = mymodel.find(params[:id])
+    object_selected.destroy
+    flash[:status] = "#{mymodel.name} \"#{object_selected.name}\" has been deleted."
+    redirect_to :action => 'index', :project_id => @project.id
+  end
   
 end
