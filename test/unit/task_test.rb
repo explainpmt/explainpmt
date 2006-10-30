581c067e893c0f7310cfe9707249882f320dfe25
Jake Dempsey <angelo0000@gmail.com>
2006-10-30 00:32:44 +0000

    added some unit tests for tasks
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@267 1861e259-8220-0410-84d7-1f84cab42028

--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -2,9 +2,35 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class TaskTest < Test::Unit::TestCase
   fixtures ALL_FIXTURES
-
+  
+  def setup
+    @task_one = Task.find 1
+    @task_two = Task.find 2
+    @cloned_task = @task_one.clone
+  end
+  
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_story_association
+    assert_equal @task_one.story,  Story.find( 1 ) 
+  end
+  
+  def test_user_association
+    assert_equal @task_one.owner, User.find( 1 )
+  end
+  
+  def test_uniqueness_of_task_name
+    @cloned_task.save
+    assert @cloned_task.errors.on(:name)
+  end
+  
+  def test_same_task_name_on_separate_projects
+    @cloned_task.story = Story.find 2
+    @cloned_task.save
+    assert :success
+  end
+  
+  def test_validate_presence_of_name
+    task = Task.create
+    assert task.errors.on(:name)
   end
 end
