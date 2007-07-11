a9a61f616b0e93d986e5178e7e3180fad65b895e
Jake Dempsey <angelo0000@gmail.com>
2007-07-11 03:22:14 +0000

    housekeeping
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@439 1861e259-8220-0410-84d7-1f84cab42028

--- app/controllers/acceptancetests_controller.rb
+++ app/controllers/acceptancetests_controller.rb
@@ -9,28 +9,24 @@ class AcceptancetestsController < ApplicationController
   end
   
   def clone_acceptancetest
-	@object = session[:object] || mymodel.find(params[:id])
-	if(params[:story_id])
-		@story = Story.find(params[:story_id])
-	end
-    session[:object] = nil
+    @object = mymodel.find params[:id]
+	  @story = Story.find(params[:story_id]) if params[:story_id]
     @page_title = "Clone #{mymodel}"
   end
   
   def index
     @stories = @project.stories
-    @acceptancetests = mymodel.find_all_acceptance_tests(@project.id)
+    @acceptancetests = mymodel.find_all_acceptance_tests @project.id
     @page_title = "Acceptance Tests"
   end
   
   def new_acceptance_for_story
-    @object = session[:object] || Acceptancetest.new
-    session[:object] = nil
-    @story = Story.find(params[:story_id])
+    @object = Acceptancetest.new
+    @story = Story.find params[:story_id]
   end
   
   def delete_from_story
-    myobject = mymodel.find(params[:id])
+    myobject = mymodel.find params[:id]
     myobject.destroy
     flash[:status] = "#{mymodel.name} \"#{myobject.name}\" has been deleted."
     redirect_to :controller => 'stories', :action => 'show', 
