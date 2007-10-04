95ffe6fe8f2cf3a2b0e622149ded7cecb43e5289
Jake Dempsey <angelo0000@gmail.com>
2007-10-04 05:56:50 +0000

    oops
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@506 1861e259-8220-0410-84d7-1f84cab42028

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -2,5 +2,17 @@ class Milestone < ActiveRecord::Base
   belongs_to :project
   validates_presence_of :name, :date
   validates_length_of :name, :maximum => 100
+  
+  def future?
+    date >= Date.today
+  end
+  
+  def recent?
+    date < Date.today && date > Date.today - 15
+  end
+  
+  def past?
+    date < Date.today
+  end
 end
 
