5ec2575966b2476e68d17a84d91000f62f5d88aa
Jake Dempsey <angelo0000@gmail.com>
2007-10-04 05:39:00 +0000

    removed useless custom finder, just made an association on the model instead
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@500 1861e259-8220-0410-84d7-1f84cab42028

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,8 +3,4 @@ class Task < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User', :foreign_key => 'user_id'
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => "story_id"
-    
-  def self.find_all_by_user_and_project(user, project)
-    Task.find_by_sql ["SELECT * FROM tasks where user_id = ? AND story_id in (select id from stories where project_id = ? AND status not in (7,8))",user.id, project.id]
-  end
 end
