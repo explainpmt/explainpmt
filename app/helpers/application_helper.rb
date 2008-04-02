e9a78ad2eee62f76a726ddcc9d94ef7e639014a4
Jake Dempsey <angelo0000@gmail.com>
2008-04-02 22:01:28 +0000

    removed some dead helper code
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@643 1861e259-8220-0410-84d7-1f84cab42028

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,8 @@ module ApplicationHelper
   include TasksHelper
   include UsersHelper
 
+  VERSION = 'dev trunk'
+
   def admin_content(&block)
     yield if is_admin?
   end
@@ -53,16 +55,13 @@ module ApplicationHelper
 
     # Print window pages
     first.upto(last) do |page|
-      (current_page == page && !link_to_current_page) ? html << page : html << yield(page)
+     (current_page == page && !link_to_current_page) ? html << page : html << yield(page)
     end
 
     # Print end page if anchors are enabled
     html << yield(pagingEnum.last_page) if always_show_anchors and not last == pagingEnum.last_page
     html
   end
-
-
-  VERSION = 'dev trunk'
 
   def is_admin?
     current_user && current_user.admin
@@ -72,8 +71,16 @@ module ApplicationHelper
     current_user.projects.select { |p| p != @project }
   end
 
-  def page_title
-    @project ? "#{@project.name} &raquo; #{@page_title}" : @page_title
+  def long_date(date)
+    date.strftime('%A %B %d %Y')
+  end
+
+  def short_date(date)
+    date.strftime('%a %b %d, %y')
+  end
+
+  def numeric_date(date)
+    date.strftime('%m/%d/%Y')
   end
 
   def top_menu
@@ -89,14 +96,14 @@ module ApplicationHelper
       if is_admin?
         xml.li(:class => 'right') do
           xml << main_menu_link('Users', :controller => 'users',
-            :action => 'index') {
+          :action => 'index') {
             @project.nil? and controller.controller_name == 'users'
           }
         end
       end
       xml.li(:class => 'right') do
         xml << main_menu_link('Projects', :controller => 'projects',
-          :action => 'index')
+        :action => 'index')
       end
     end
   end
@@ -110,41 +117,41 @@ module ApplicationHelper
         end
         xml.li do
           xml << main_menu_link('Releases', :controller => 'releases',
-            :action => 'index',
-            :project_id => @project.id)
+          :action => 'index',
+          :project_id => @project.id)
         end
         xml.li do
           xml << main_menu_link('Iterations', :controller => 'iterations',
-            :action => 'index',
-            :project_id => @project.id)
+          :action => 'index',
+          :project_id => @project.id)
         end
         xml.li do
           xml << main_menu_link('Backlog', :controller => 'stories',
-            :action => 'index',
-            :project_id => @project.id)
+          :action => 'index',
+          :project_id => @project.id)
         end
         xml.li do
           xml << main_menu_link('Initiatives', :controller => 'initiatives',
-            :action => 'index',
-            :project_id => @project.id)
+          :action => 'index',
+          :project_id => @project.id)
         end
         xml.li do
           xml << main_menu_link('Acceptance Tests', :controller => 'acceptancetests',
-            :action => 'index',
-            :project_id => @project.id)
+          :action => 'index',
+          :project_id => @project.id)
         end
         xml.li do
           xml << main_menu_link('Milestones', :controller => 'milestones',
-            :action => 'index',
-            :project_id => @project.id)
+          :action => 'index',
+          :project_id => @project.id)
         end
         xml.li do
           xml << main_menu_link('Team', team_project_path(@project))
         end
         xml.li do
           xml << main_menu_link('Stats', :controller => 'stats',
-            :action => 'index',
-            :project_id => @project.id)
+          :action => 'index',
+          :project_id => @project.id)
         end
       end
     end
@@ -152,45 +159,15 @@ module ApplicationHelper
 
   def main_menu_link(title, options)
     selected_controller = @selected_main_menu_link ?
-      @selected_main_menu_link : controller.controller_name
+    @selected_main_menu_link : controller.controller_name
     if (block_given? and yield) or
-        (!block_given? and selected_controller.to_s == options[:controller].to_s)
+     (!block_given? and selected_controller.to_s == options[:controller].to_s)
 
       html_options = { 'class' => 'current' }
     else
       html_options = {}
     end
     link_to(title,options,html_options)
-  end
-
-  def long_date(date)
-    date.strftime('%A %B %d %Y')
-  end
-
-  def short_date(date)
-    date.strftime('%a %b %d, %y')
-  end
-
-  def numeric_date(date)
-    date.strftime('%m/%d/%Y')
-  end
-
-  def collection_select_with_current(object, method, collection, value_method, text_method, current_value)
-    result = "<select name='#{object}[#{method}]'>\n"
-    if current_value == nil
-      result << "<option value=''selected='selected'></option>"
-    else
-      result << "<option value=''></option>"
-    end
-    for element in collection
-      if current_value == element.send(value_method)
-        result << "<option value='#{element.send(value_method)}' selected='selected'>#{element.send(text_method)}</option>\n"
-      else
-        result << "<option value='#{element.send(value_method)}'>#{element.send(text_method)}</option>\n"
-      end
-    end
-    result << "</select>\n"
-    return result
   end
 
 end
