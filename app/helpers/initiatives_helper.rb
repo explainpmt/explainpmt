b98b66ce78413201e9b4b5d727f5a372fc0ab780
Josh <josht323@gmail.com>
2008-04-10 17:37:37 +0000

    converting initiatives to use new action select menu
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@660 1861e259-8220-0410-84d7-1f84cab42028

--- app/helpers/initiatives_helper.rb
+++ app/helpers/initiatives_helper.rb
@@ -2,12 +2,16 @@ module InitiativesHelper
   def link_to_new_initiative
     link_to_remote 'New Initiative', :url => new_project_initiative_path(@project), :method => :get
   end
-  
+
   def link_to_edit_initiative(initiative, options={})
     link_to_remote(options[:value] || initiative.name, :url => edit_project_initiative_path(initiative.project, initiative), :method => :get)
   end
-  
-  def link_to_delete_initiative(initiative)
-    link_to "Delete", project_initiative_path(@project, initiative), :method => :delete, :confirm => "Are you sure you want to delete?"
+
+  def option_to_edit_initiative(initiative, options={})
+    create_action_option(options[:value] || initiative.name, edit_project_initiative_path(initiative.project, initiative))
+  end
+
+  def option_to_delete_initiative(initiative)
+    create_action_option("Delete", project_initiative_path(@project, initiative), :method => :delete, :confirm => "Are you sure you want to delete?")
   end
 end
