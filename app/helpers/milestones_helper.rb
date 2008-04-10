35d8a7b5d2874a1d5c3d50724bf654f04847e70c
Josh <josht323@gmail.com>
2008-04-10 17:43:51 +0000

    converting milestones to use new action select menu
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@661 1861e259-8220-0410-84d7-1f84cab42028

--- app/helpers/milestones_helper.rb
+++ app/helpers/milestones_helper.rb
@@ -4,33 +4,33 @@ module MilestonesHelper
   end
 
   def link_to_edit_milestone(milestone, options={})
-    link_to_remote(options[:value] || milestone.name, :url => edit_project_milestone_path(@project, milestone), :method => :get)
+    create_action_option(options[:value] || milestone.name, edit_project_milestone_path(@project, milestone))
   end
-  
+
   def link_to_delete_milestone(milestone)
-    link_to "Delete", project_milestone_path(@project, milestone), :method => :delete, :confirm => "Are you sure you want to delete?"
+    create_action_option("Delete", project_milestone_path(@project, milestone), :method => :delete, :confirm => "Are you sure you want to delete?")
   end
-  
+
   def link_to_show_all_milestones
     link_to_remote 'show all', :url => show_all_project_milestones_path(@project), :method => :get
   end
-  
+
   def link_to_show_recent_milestones
     link_to_remote 'show recent', :url => show_recent_project_milestones_path(@project), :method => :get
   end
-  
+
   def milestone_list_for(milestones, table_id)
     render :partial => 'milestones', :locals => {:milestones => milestones, :table_id => table_id }
   end
-  
+
   def past_milestones
     milestone_list_for @project.milestones.past, 'past_milestones'
   end
-  
+
   def recent_milestones
     milestone_list_for @project.milestones.recent, 'recent_milestones'
   end
-  
+
   def link_to_show_milestone(milestone, options={})
     link_to_remote(options[:value] || milestone.name, :url => project_milestone_path(milestone.project, milestone), :method => :get)
   end
