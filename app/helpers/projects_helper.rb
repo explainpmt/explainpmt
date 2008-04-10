e3495e29646ce0c648525e6c2094cea9d7a9784b
Josh <josht323@gmail.com>
2008-04-10 18:12:07 +0000

    converting projects to use new action select menu
    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@663 1861e259-8220-0410-84d7-1f84cab42028

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,17 +2,25 @@ module ProjectsHelper
   def empty_projects_content(&block)
     yield if @projects.empty?
   end
-  
+
   def project_audit_image(project)
     link_to image_tag("xml.gif"), audits_project_path(project)
   end
-  
+
   def project_dashboard_link(project)
     link_to project.name, project_dashboard_path(project)
   end
-  
-  def link_to_delete_project(project)
-    link_to "Delete", project_path(project), :method => :delete, :confirm => "Are you sure you want to delete?\r\nAll associated data will also be deleted. This action can not be undone."
+
+  def option_to_view_team(project)
+    create_action_option("View Team", project_users_path(project))
+  end
+
+  def option_to_edit_project(project)
+    create_action_option("Edit", edit_project_path(project))
+  end
+
+  def option_to_delete_project(project)
+    create_action_option("Delete", project_path(project), :method => :delete, :confirm => 'Are you sure you want to delete?\r\nAll associated data will also be deleted. This action can not be undone.')
   end
 
   def link_to_remove_user(user)
