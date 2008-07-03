3fb7f5d60129afcf6c40c410cc3514dcfeae5952
Jake Dempsey <angelo0000@gmail.com>
2008-07-03 14:39:57 +0000

    git-svn-id: http://explainpmt.googlecode.com/svn/trunk@719 1861e259-8220-0410-84d7-1f84cab42028

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
-
+RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
