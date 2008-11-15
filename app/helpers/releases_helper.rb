explainpmt/explainpmt

commit 7c256e3e959372875f7a83eeec7f5a31bc700d3c

More work on the Release module.  Can now add and remove stories to/from a release from the Release page. Also updated the audit icon to be the standard RSS icon rather than the existing XML icon now that Jen has XML-ified everything properly.

app/helpers/releases_helper.rb
@@ -10,6 +10,10 @@ module ReleasesHelper
   def link_to_edit_release(release, options={})
     link_to_remote(options[:value] || release.name, :url => edit_project_release_path(@project, release), :method => :get)
   end
+  
+  def link_to_assign_stories(release)
+    link_to_remote('Assign Story Cards', :url => select_stories_project_release_path(@project, release), :method => :get)
+  end
 
   def option_to_edit_release(release)
     create_action_option("Edit", edit_project_release_path(@project, release))

app/views/releases/show.rhtml
@@ -2,6 +2,7 @@
     (<%= link_to_edit_release(@release, :value=>"Edit") %>)
     <br/>
 </div>
+
 <h3><%= @release.name %></h3>
 <div id="IterationSummary">
     <%= render :partial => 'release_summary' %>
@@ -17,12 +18,16 @@
 <%= textilize(@release.description) %>
 <% end %>
 <br/>
+(<%= link_to_assign_stories(@release) %>)
 <% empty_collection_content(@stories) do %>
 <p>
     No stories are in this release yet.
 </p>
 <% end %>
 <% collection_content(@stories) do %>
+    <%= form_tag remove_stories_project_release_path(@project) %>
+<div class="move_to">Remove Selected Stories from Release? <%= submit_tag "Remove" %></div>
     <%= render( :partial => 'stories/stories', :locals => {:hide_cols => [ :project, :release]}) %>
+</form>
   <% end %>
 

app/controllers/releases_controller.rb
@@ -52,9 +52,23 @@ class ReleasesController < ApplicationController
       page.redirect_to project_releases_path(@project)
     end
   end
+  
+  def select_stories
+    find_stories_not_in_release
+    @stories = @other_stories
+    render :update do |page|
+      page.call 'showPopup', render(:partial => 'select_stories')
+    end
+  end
 
-  def assign_story
-    change_story_release
+  def assign_stories
+    change_story_release(Release.find_by_id(params[:id]))
+    redirect_to project_release_path(@project, @release)
+  end
+  
+  def remove_stories
+    change_story_release(nil)
+    redirect_to project_release_path(@project, @release)
   end
 
   protected
@@ -64,15 +78,14 @@ class ReleasesController < ApplicationController
   end
   
   def find_stories_not_in_release
-     @other_stories = @project.stories.backlog.select { |s|
+     @other_stories = @project.stories.select { |s|
       s.release_id.nil?
     }
   end
   
-  def change_story_release
-    release = Release.find_by_id(params[:id])
-    story = Story.find(params[:story])
-    set_status_and_error_for(Story.assign_to_release(release, story))
+  def change_story_release(release)
+    stories = Story.find(params[:selected_stories]||[])
+    set_status_and_error_for(Story.assign_many_to_release(release, stories))
   end
 
   def common_popup(url)

app/helpers/projects_helper.rb
@@ -4,7 +4,11 @@ module ProjectsHelper
   end
 
   def project_audit_image(project)
-    link_to image_tag("xml.gif"), audits_project_path(project)
+    link_to image_tag("rss_small.png"), audits_project_path(project)
+  end
+  
+  def project_export_image(project)
+    link_to image_tag("xml.gif"), xml_export_project_path(project)
   end
 
   def project_dashboard_link(project)

app/models/story.rb
@@ -190,14 +190,16 @@ class Story < ActiveRecord::Base
     {:successes => successes, :failures => failures}
   end
   
-  def self.assign_to_release(release, story)
+  def self.assign_many_to_release(release, stories)
     successes, failures = [], []
-    story.release = release
+    stories.each do |s|
+      s.release = release
       if s.save
         successes << "SC#{s.scid} has been moved."
       else
         failures << "SC#{s.scid} could not be moved."
       end
+    end
     {:successes => successes, :failures => failures}
   end
 

app/views/iterations/_select_stories.rhtml
@@ -3,7 +3,6 @@
   <h3>Select the stories that you want to add to the iteration.</h3><br />
   <%= form_tag assign_stories_project_iteration_path(@project, @iteration) %>
     <%= hidden_field_tag :move_to, @iteration.id %>
-    <input type="hidden" name="move_to" value="<%= @iteration.id %>" />
     <%= render( :partial => 'stories/stories', :locals => {:hide_cols => [ :owner, :action, :project, :iteration ] } ) %>
   	<div class="submit"><%= submit_tag "Add Selected Stories to Iteration" %></div>
   </form>

app/views/releases/_select_stories.rhtml
@@ -1,0 +1,9 @@
+<div class="select_stories">
+  <a href="javascript:void(0);" class="close" onclick="cClick();">close</a>
+  <h3>Select the stories that you want to add to the release.</h3><br />
+  <%= form_tag assign_stories_project_release_path(@project, @release) %>
+    <%= hidden_field_tag :move_to, @release.id %>
+    <%= render( :partial => 'stories/stories', :locals => {:hide_cols => [ :owner, :action, :project, :release ] } ) %>
+  	<div class="submit"><%= submit_tag "Add Selected Stories to Release" %></div>
+  </form>
+</div>

config/routes.rb
@@ -7,7 +7,7 @@ map.resources :projects, :member => {:xml_export => :get, :audits => :get, :team
   project.resources :acceptancetests, :member => {:clone_acceptance => :get}, :collection => {:export => :get, :assign => :post,}
   project.resource :dashboard
   project.resources :users, :member => {:remove_from_project => :put}
-  project.resources :releases do |release|
+  project.resources :releases, :member=>{:select_stories => :get, :assign_stories => :post, :remove_stories=>:post} do |release|
     release.resources :stories
   end
   project.resources :stats

commit 9f9ba1c4a5b68ee4d87ba6f7bf2f5c55463b0347

Fixes to release/iteration module Issues introduced in 7c256e3e959372875f7a83eeec7f5a31bc700d3c related to release helper.  Corrected.  Was impacting ability to assign stories from the iteration window.

app/helpers/releases_helper.rb
@@ -11,7 +11,7 @@ module ReleasesHelper
     link_to_remote(options[:value] || release.name, :url => edit_project_release_path(@project, release), :method => :get)
   end
   
-  def link_to_assign_stories(release)
+  def link_to_assign_release_stories(release)
     link_to_remote('Assign Story Cards', :url => select_stories_project_release_path(@project, release), :method => :get)
   end
 

app/views/releases/show.rhtml
@@ -18,7 +18,7 @@
 <%= textilize(@release.description) %>
 <% end %>
 <br/>
-(<%= link_to_assign_stories(@release) %>)
+(<%= link_to_assign_release_stories(@release) %>)
 <% empty_collection_content(@stories) do %>
 <p>
     No stories are in this release yet.