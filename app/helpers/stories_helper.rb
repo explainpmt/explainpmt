cf60b23be0e18f7d1c8bfc2e53c5097bad878df7
Brandon Aaron <brandon.aaron@gmail.com>
2010-06-10 09:57:17 -0500

    truncate takes an options hash for the second argument

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -19,7 +19,7 @@ module StoriesHelper
   end
 
   def link_to_story_with_sc(story)
-    link_to("SC#{story.scid}", project_story_path(@project, story)) + '(' + truncate(story.title, 30) + ')'
+    link_to("SC#{story.scid}", project_story_path(@project, story)) + '(' + truncate(story.title, :length => 30) + ')'
   end
 
   def link_to_story(story, options={})
@@ -51,7 +51,7 @@ module StoriesHelper
   end
 
   def story_select_list_for(stories)
-    stories.inject(""){|options, story| options << "<option value='#{story.id}'>SC#{story.scid}  (#{truncate(story.title,30)})</option>"}
+    stories.inject(""){|options, story| options << "<option value='#{story.id}'>SC#{story.scid}  (#{truncate(story.title, :length => 30)})</option>"}
   end
 
   def link_to_edit_story(story, options={})
