f9ddb9d0d742c51c8e2d17ef6d7bf06221d35488
Jen Wendling <jenniferwendling@gmail.com>
2010-12-17 00:52:30 -0600

    adding project dashboard view

---
+++ app/helpers/dashboard_helper.rb
@@ -0,0 +1,17 @@
+module DashboardHelper
+  def milestones_calendar(days_to_render=14)
+    title_prefix = 'Upcoming Milestones:'
+    @calendar_title = @project ? title_prefix : title_prefix.gsub(':', ' (all projects):')
+    milestones = (@project ? @project.milestones : current_user.milestones).select { |m|
+      m.date >= Date.today && m.date < Date.today + days_to_render
+    }
+
+    @days = Array.new(days_to_render) {|index|
+      current_day = Date.today + index
+      {:date => current_day,
+        :name => Date::DAYNAMES[current_day.wday],
+        :milestones => milestones.select { |m| m.date == current_day }}
+    }
+    render :partial => 'milestones/calendar'
+  end
+end
